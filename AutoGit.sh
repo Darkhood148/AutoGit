be89e8432bc38995a6be8acde50c03572e858071
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2023-05-21 21:37:59 +0530

    N/A

--- AutoGit.sh
+++ AutoGit.sh
@@ -15,7 +15,7 @@ function enter_credentials {
         echo $token > $file_name
         echo "Enter username associated with this token"
         read username
-        echo $username >> $file_name
+        echo $username > $user_file
 }
 
 printf "~~~~~~~~Welcome To AutoGit~~~~~~~~\n\n"
@@ -29,23 +29,25 @@ do
     read choice
     if [[ choice -eq 1 ]]; then
         file_name=".active_access_token.txt"
-        if [[ -f $file_name ]]; then
+        user_file=".active_user_name.txt"
+        if [[ -f $file_name && -f $user_file ]]; then
             token=$(cat $file_name)
         else
-            echo "Looks like you don't have any token."
+            echo "Looks like you are missing something."
             enter_credentials
         fi
         echo "Enter repo name"
         read repo_name
         echo "Enter repo description"
         read repo_description
+        printf $token
         printf "\nCreating repo\n====================\n\n"
         curl -H "Authorization: token $token" --data "{\"name\":\"$repo_name\", \"description\":\"$repo_description\"}" https://api.github.com/user/repos
         printf "\n\n====================\nDone\n"
         echo "Enter \"y\" to make first commit."
         read ch
         if [[ $ch == "y" ]]; then
-            username=$(cat $file_name | xargs | awk '{print $2}')
+            username=$(cat $user_file)
             push_to_git $username $repo_name
         fi
     fi
